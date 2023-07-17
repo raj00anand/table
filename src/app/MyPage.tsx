041cfb685fd425a2caa1d806f6fbaf58460200da
Anand Raj <ar.anandraj02@gmail.com>
2023-07-17 23:17:55 +0530

    initial commit

---
+++ src/app/MyPage.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const MyPage: React.FC = () => {
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default MyPage;
